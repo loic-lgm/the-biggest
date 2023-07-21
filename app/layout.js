4e6d32f8e9aa9cf50be9e3db9dba5d76b7dc54e3
John Hash <johnhash@frozen.com>
2023-07-21 20:16:16 +0200

    Update navbar

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,7 @@
 import '../styles/globals.css'
 import { Inter } from 'next/font/google'
 import { Poppins } from 'next/font/google'
+import Navbar from "@/components/navbar";
 
 const inter = Inter({ subsets: ['latin'] })
 const poppins = Poppins({ 
@@ -16,7 +17,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={poppins.className}>{children}</body>
+      <body className={poppins.className}>
+        <Navbar />
+        {children}
+      </body>
     </html>
   )
 }
--- app/page.js
+++ app/page.js
@@ -1,10 +1,8 @@
-import Navbar from "@/components/navbar";
 import HomePage from "@/components/home_page";
 
 export default function Home() {
   return (
     <>
-    <Navbar />
     <HomePage />
     </>
   )
