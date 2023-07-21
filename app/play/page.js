febdbb6722bdf75ef11f9eed497964f23642e173
John Hash <johnhash@frozen.com>
2023-07-21 21:59:53 +0200

    Add card

---
+++ app/play/page.js
@@ -0,0 +1,11 @@
+import Card from "@/components/card";
+import '../../styles/play.css'
+
+export default function Play() {
+	return (
+		<div className="flex container-card">
+			<Card />
+			<Card />
+		</div>
+	)
+}
---
+++ components/card.js
@@ -0,0 +1,15 @@
+export default function Card() {
+	return (
+		<div className="flex flex-col justify-around items-center w-1/2 hover:bg-orange-100 hover:cursor-pointer">
+			<div className="w-3/4 mt-8">
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero accusantium, voluptas aspernatur iure laboriosam cum vitae iusto ad dolore nostrum commodi rerum ullam natus atque tempore ab nesciunt sed aliquam vel, delectus sint obcaecati architecto maiores. Repellendus rerum adipisci soluta illo natus officiis numquam cum minima quos recusandae quod atque, consequatur voluptate ipsa error similique iure fugiat, tenetur est reiciendis. Eum ea esse magnam quae cupiditate soluta iusto, dignissimos culpa quod, quia porro dolorem exercitationem itaque reprehenderit optio reiciendis! Dolores suscipit eos assumenda sed provident quae blanditiis, adipisci, minima fugit ea ratione doloremque iste, optio repudiandae rerum velit molestias vero.
+			</div>
+			<div>
+				Année du crime
+			</div>
+			<div>
+				Pays du crime
+			</div>
+		</div>
+	)
+}
